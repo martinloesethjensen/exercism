0efc078ab809f8c49a5e58f44cebf6e6effe6667
Martin Jensen <martinloesethjensen@gmail.com>
2023-01-18 12:20:12 +0100

    Formatting and typo

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,19 +1,16 @@
 class Bob {
   String response(String s) {
     final q = s.trim();
+
     if (q.isEmpty) return 'Fine. Be that way!';
 
     if (q.isQuestion && q.isUpperCase) {
       return 'Calm down, I know what I\'m doing!';
     }
 
-    if (q.isQuestion) {
-      return 'Sure.';
-    }
+    if (q.isQuestion) return 'Sure.';
 
-    if (q.isUpperCase) {
-      return 'Whoa, chill out!';
-    }
+    if (q.isUpperCase) return 'Whoa, chill out!';
 
     return 'Whatever.';
   }
@@ -22,7 +19,7 @@ class Bob {
 extension on String {
   bool get isQuestion => endsWith('?');
 
-  /// check if string contains and letters and if it is upper case.
+  /// check if string contains any letters and if it is upper case.
   bool get isUpperCase =>
       contains(RegExp(r'[A-Za-z]')) && this == this.toUpperCase();
 }
