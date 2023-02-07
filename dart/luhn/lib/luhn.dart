39bbb7499e07a5a2c8acdd0be59528ae6fc5969b
Martin Jensen <martinloesethjensen@gmail.com>
2023-02-07 07:11:36 +0100

    working luhn algorithm

--- dart/luhn/lib/luhn.dart
+++ dart/luhn/lib/luhn.dart
@@ -1,16 +1,32 @@
+import 'package:collection/collection.dart';
+
 class Luhn {
-  final exp = RegExp(r'^\d+$');
+  final nonDigitExp = RegExp(r'^\d+$');
+  final nonSpaceExp = RegExp(r'\s+');
   bool valid(String s) {
-    final str = 'acb'.replaceAll(' ', '').split('').reversed.join().trim();
+    /// Strip spaces
+    final str = s.replaceAll(nonSpaceExp, '');
 
-    if (s.length <= 1) return false;
+    if (str.length <= 1) return false;
 
-    RegExpMatch? match = exp.firstMatch(str);
-
+    /// Should not contain any non digits
+    RegExpMatch? match = nonDigitExp.firstMatch(str);
     if (match == null) return false;
 
-    print(match[0]);
+    var isDouble = true;
+    final _result = match
+            .group(0)
+            ?.split('')
+            .reversed
+            .map((e) => int.parse(e))
+            .mapIndexed((index, number) {
+          isDouble = !isDouble;
+          if (!isDouble) return number;
+          final doubled = number * 2;
+          return doubled > 9 ? doubled - 9 : doubled;
+        }).fold(0, (previousValue, element) => previousValue + element) ??
+        0;
 
-    return false;
+    return _result % 10 == 0;
   }
 }
