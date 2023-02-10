dcb6835db67999ef1f275806d39324815059b03c
Martin Jensen <martinloesethjensen@gmail.com>
2023-02-10 10:54:55 +0100

    Uploaded dart isogram submission

--- dart/isogram/lib/isogram.dart
+++ dart/isogram/lib/isogram.dart
@@ -1,10 +1,8 @@
 class Isogram {
   bool isIsogram(String s) {
-    final match = RegExp(r'[\w- ]+').firstMatch(s)?.group(0) ?? '';
-    final cleaned = match
-        .replaceAll(RegExp(r'[\s-]+'), '')
-        .split('')
-        .map((char) => char.toLowerCase());
+    final match =
+        RegExp(r'[\w- ]+').firstMatch(s.toLowerCase())?.group(0) ?? '';
+    final cleaned = match.replaceAll(RegExp(r'[\s-]+'), '').split('');
 
     return match.isEmpty || cleaned.toSet().length == cleaned.length;
   }
