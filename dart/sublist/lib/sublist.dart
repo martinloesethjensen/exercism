3e672f68e66cda59d12819c15eb5c9446004da4c
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-13 14:35:39 +0100

    wip solution

--- dart/sublist/lib/sublist.dart
+++ dart/sublist/lib/sublist.dart
@@ -5,11 +5,13 @@ enum Classification { sublist, superlist, equal, unequal }
 
 class Sublist {
   Classification sublist(List<int> listOne, List<int> listTwo) {
+    final listOneAsString = listOne.join(',');
+    final listTwoAsString = listTwo.join(',');
     if (ListEquality().equals(listOne, listTwo)) {
       return Classification.equal;
-    } else if (listTwo.isEmpty || listTwo.every((e) => listOne.contains(e))) {
+    } else if (listTwo.isEmpty || listOneAsString.contains(listTwoAsString)) {
       return Classification.superlist;
-    } else if (listOne.isEmpty || listOne.every((e) => listTwo.contains(e))) {
+    } else if (listOne.isEmpty || listTwoAsString.contains(listOneAsString)) {
       return Classification.sublist;
     }
     return Classification.unequal;
