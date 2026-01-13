57adc9e8c086eded577d712adb768386bbbe3e9c
Martin Jensen <martinloesethjensen@gmail.com>
2026-01-13 19:19:57 +0100

    solution with sliding window logic and loop labelling

--- dart/sublist/lib/sublist.dart
+++ dart/sublist/lib/sublist.dart
@@ -1,19 +1,41 @@
-// Implement the Classification enum.
 import 'package:collection/collection.dart';
 
 enum Classification { sublist, superlist, equal, unequal }
 
 class Sublist {
   Classification sublist(List<int> listOne, List<int> listTwo) {
-    final listOneAsString = listOne.join(',');
-    final listTwoAsString = listTwo.join(',');
     if (ListEquality().equals(listOne, listTwo)) {
       return Classification.equal;
-    } else if (listTwo.isEmpty || listOneAsString.contains(listTwoAsString)) {
+    } else if (listOne.partialContains(listTwo)) {
       return Classification.superlist;
-    } else if (listOne.isEmpty || listTwoAsString.contains(listOneAsString)) {
+    } else if (listTwo.partialContains(listOne)) {
       return Classification.sublist;
     }
     return Classification.unequal;
+  }
+}
+
+extension ListExt<T> on List<T> {
+  bool partialContains(List<T> other) {
+    // Custom rule to return true if other is empty
+    if (other.isEmpty) return true;
+
+    final otherLength = other.length;
+
+    if (otherLength > length) return false;
+
+    outerLoop:
+    for (var index = 0; index <= length; index++) {
+      if ((length - index) < otherLength) break;
+
+      for (var i = 0; i < otherLength; i++) {
+        if (this[index + i] != other[i]) {
+          continue outerLoop;
+        }
+      }
+      return true;
+    }
+
+    return false;
   }
 }
