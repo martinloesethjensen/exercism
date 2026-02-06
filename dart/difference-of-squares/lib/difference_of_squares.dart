f95db57abfe0d884f866b517256229c0053c7151
Martin Jensen <martinloesethjensen@gmail.com>
2026-02-06 21:55:04 +0100

    difference of squares refactor again

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,18 +1,16 @@
-import 'dart:math';
-
 class DifferenceOfSquares {
   num squareOfSum(int n) {
     var sum = 0;
     for (var i = 1; i <= n; i++) {
       sum += i;
     }
-    return pow(sum, 2);
+    return sum * sum;
   }
 
   num sumOfSquares(int n) {
     var sum = 0;
     for (var i = 1; i <= n; i++) {
-      sum += pow(i, 2).toInt();
+      sum += (i * i);
     }
     return sum;
   }
