13d58d3b77e8423193317bae437d8169d639c025
Martin Jensen <martinloesethjensen@gmail.com>
2026-02-07 15:23:01 +0100

    binary search

--- dart/binary-search/lib/binary_search.dart
+++ dart/binary-search/lib/binary_search.dart
@@ -1,5 +1,28 @@
 import 'package:binary_search/value_not_found_exception.dart';
 
 class BinarySearch {
-  // Your code here.
+  BinarySearch(this.list);
+
+  final List<int> list;
+
+  int find(int target) {
+    if (list.isEmpty) throw ValueNotFoundException('$target');
+
+    var low = 0;
+    var high = list.length - 1;
+
+    while (low <= high) {
+      final mid = (low + high) ~/ 2;
+      final item = list[mid];
+      if (target == item) return mid;
+      if (target < item) {
+        high = mid - 1;
+      }
+      if (target > item) {
+        low = mid + 1;
+      }
+    }
+
+    throw ValueNotFoundException('$target');
+  }
 }
